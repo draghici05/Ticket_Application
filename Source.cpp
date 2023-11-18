ef38f2a10b5a5661bf51068992e19711d3a4aa8e
draghici05 <147331173+draghici05@users.noreply.github.com>
2023-11-18 22:32:38 +0200

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Location {
+
+	int MaxNoSeat;
+	int *NoRows;
+	string Zone;
+
+};
+
+
+class Event {
+
+	string Name;
+	int Time;
+	char Date;
+
+};
+
+class Ticket {
+
+	string Type;
+	int Price;
+
+};
