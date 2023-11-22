9f09e15b58fd8eb6c0a4af78ea35b514cf1295d0
draghici05 <alexdraghici190@gmail.com>
2023-11-22 23:53:50 +0200

    Updated the classes details, added constructors for each class + functions to show details of each class.

--- TicketingApp/TicketingApp/Source.cpp
+++ TicketingApp/TicketingApp/Source.cpp
@@ -5,32 +5,41 @@ class Location {
 public:
 	int MaxNoSeat;
 	int *NoRows;
-	string Zone;
-
-	Location(int maxnoseat, int* norows, string zone) {
+	const char* City;
+	
+	Location(int maxnoseat, int* norows, const char* city) {
 		MaxNoSeat = maxnoseat;
 		NoRows = norows;
-		Zone = zone;
+		City = city;
+		
 	}
 
+	void locationDetails() {
+		std::cout << "The event takes place in: " << City << std::endl;
+		std::cout << "The maximum ammount of seats in this location is: " << MaxNoSeat << std::endl;
+		std::cout << "The number of rows in this location is: " << NoRows << std::endl;
+	}
 };
 
 class Event {
 public:
-	string Name;
+	const char* EventName;
+	const Location& location;
 	string Time;
-	string Date;
+	const char* Date;
 
-	void EventPresentation() {
-		std::cout << "Name of the event - " << Name << std::endl;
-		std::cout << "The date on which the event takes place - " << Date << std::endl;
-		std::cout << "The time when the event starts - " << Time << std::endl;
-	}
 
-	Event(string name, string date, string time) {
-		Name = name;
+	Event(const char* name, const char* date, string time, const Location& location) {
+		EventName = name;
 		Time = time;
 		Date = date;
+		Location = location;
+	}
+
+	void eventPresentation() {
+		std::cout << "Name of the event - " << EventName << std::endl;
+		std::cout << "The date on which the event takes place - " << Date << std::endl;
+		std::cout << "The time when the event starts - " << Time << std::endl;
 	}
 
 };
@@ -38,19 +47,21 @@ public:
 class Ticket {
 public:
 	string Type;
-	int Price;
+	const char* EventName;
+	int SeatNumber;
+	double Price;
 
-	Ticket(string type, int price) {
+	Ticket(string type, const char* eventName, int seatNumber, double price) {
 		Type = type;
 		Price = price;
+		EventName = eventName;
+		SeatNumber = seatNumber;
+	}
+
+	void ticketDetails() {
+		std::cout << "The ticket is for the event: " << EventName << std::endl;
+		std::cout << "The seat number is: " << SeatNumber << std::endl;
+		std::cout << "The type of bought ticket is: " << Type << std::endl;
+		std::cout << "The price of the ticket is: RON" << Price << std::endl;
 	}
 };
-
-
-int main() {
-
-	Event event1 = Event("Movie night", "20th February", "10 PM");
-	event1.EventPresentation();
-
-}
-
