051bc15088b4474272ca1c8a0d46a688a23db4c4
draghici05 <147331173+draghici05@users.noreply.github.com>
2023-12-23 23:51:50 +0200

    idk

--- TicketingApp/TicketingApp/Location.cpp
+++ TicketingApp/TicketingApp/Location.cpp
@@ -12,3 +12,58 @@ void Location::setName(string name) {
 	this->name = name;
 }
 
+void Location::addZone(Zone zone) {
+	zones.push_back(zone);
+}
+
+void Location::removeZone(string name) {
+	for (auto it = zones.begin(); it != zones.end(); ++it) {
+		if (it->getName() == name) {
+			zones.erase(it);
+			cout << "Zone: " << name << " removed succesfully " << endl;
+			return;
+		}
+	}
+	cout << "Zone " << name << " not found " << endl;
+}
+
+Zone& Location::getZone(string name) {
+	for (auto& zone : zones) {
+		if (zone.getName() == name) {
+			return zone;
+		}
+	}
+	cout << "Zone " << name << " not found. Return to the first zone " << endl;
+	return zones.front();
+}
+
+void Location::clearSeats() {
+	for (auto& zone : zones) {
+		zone.clearSeats();
+	}
+}
+
+bool Location::reserveSeat(string zoneName, unsigned int row) {
+	for (auto& zone : zones) {
+		if (zone.getName() == name) {
+			return zone.reserveSeat(row);
+		}
+	}
+	cout << "Zone " << zoneName << " not found " << endl;
+	return false;
+}
+
+ostream& operator<<(ostream& out, Location& location) {
+	out << "Location: " << location.name << endl;
+	out << "Zones:" << endl;
+	for (auto& zone : location.zones) {
+		out << zone << endl;
+	}
+	return out;
+}
+
+istream& operator>>(istream& in, Location& location) {
+	cout << "Enter location name: ";
+	in >> location.name;
+	return in;
+}
