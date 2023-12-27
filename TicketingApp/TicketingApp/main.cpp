d63cd46c86f26244dea3c660ec61b127a3c906b5
draghici05 <147331173+draghici05@users.noreply.github.com>
2023-12-27 14:38:43 +0200

    Update main.cpp

--- TicketingApp/TicketingApp/main.cpp
+++ TicketingApp/TicketingApp/main.cpp
@@ -1,5 +1,46 @@
+#include "Date.h"
 #include "Event.h"
+#include "Location.h"
+#include "Ticket.h"
+#include "Zone.h"
 
-int main(int, char**){
-    Event e("name", Date(1, 1, 1), 1, 1, "location");
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+void Menu() {
+
+	int choice;
+
+	cout << "-------------------------------";
+	cout << "-------- Ticketing App --------";
+	cout << "-------------------------------";
+
+	do {
+		cout << "1.Buy ticket\n 2.Check ticket details\n 3.Modify ticket\n 4.Exit";
+		cin >> choice;
+		switch (choice) {
+		case 0:
+			cout << "Select the event: \n";
+			break;
+		case 1:
+			cout << "The details of your ticket: \n";
+			break;
+		case 2:
+			cout << "Select the details that need to be modified: \n";
+			break;
+		case 3:
+			cout << "Exiting the app. \n";
+			break;
+		}
+	}
+
+	while (choice != 0);
+}
+
+int main() {
+	
+	Menu();
 }
