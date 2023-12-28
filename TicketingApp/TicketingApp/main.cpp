cb83f18c5151cb7d29cffc4d30a098f98004d63c
draghici05 <147331173+draghici05@users.noreply.github.com>
2023-12-28 19:18:19 +0200

    Update main.cpp

--- TicketingApp/TicketingApp/main.cpp
+++ TicketingApp/TicketingApp/main.cpp
@@ -1,43 +1,46 @@
+#include <iostream>
+#include <string>
 #include "Date.h"
 #include "Event.h"
 #include "Location.h"
 #include "Ticket.h"
 #include "Zone.h"
 
-#include <iostream>
-#include <string>
-
-
 using namespace std;
 
 void Menu() {
-
 	int choice;
+	cout << "-------------------------------\n";
+	cout << "-------- Ticketing App --------\n";
+	cout << "-------------------------------\n";
 
-	cout << "-------------------------------";
-	cout << "-------- Ticketing App --------";
-	cout << "-------------------------------";
+	cout << " 1.Buy Ticket \n";
+	cout << " 2.Check Ticket \n";
+	cout << " 3.Modify Ticket \n";
+	cout << " 4.Exit the App \n";
 
 	do {
-		cout << "1.Buy ticket\n 2.Check ticket details\n 3.Modify ticket\n 4.Exit";
 		cin >> choice;
 		switch (choice) {
-		case 0:
-			cout << "Select the event: \n";
-			break;
 		case 1:
-			cout << "The details of your ticket: \n";
+			cout << "Select the ticket: \n";
 			break;
 		case 2:
-			cout << "Select the details that need to be modified: \n";
+			cout << "The details of your ticket: \n";
 			break;
 		case 3:
-			cout << "Exiting the app. \n";
+			cout << "Select the details that need to be modified: \n";
 			break;
+		case 4:
+			cout << "Closing the app... \n";
+			exit(0);
+			break;
+		default:
+			cout << "Pick an option from the list.";
 		}
 	}
 
-	while (choice != 0);
+	while (choice != 4);
 }
 
 int main() {
