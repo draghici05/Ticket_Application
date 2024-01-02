bed0f1b0804cee35850acfb449fdb6bb12bbe175
draghici05 <147331173+draghici05@users.noreply.github.com>
2024-01-02 19:25:45 +0200

    Update Ticket.h

--- TicketingApp/TicketingApp/Ticket.h
+++ TicketingApp/TicketingApp/Ticket.h
@@ -1,26 +1,42 @@
+#pragma once
+
 #include <iostream>
 using namespace std;
 
 class Ticket {
 private:
-	static int nextId;
-	const int id;
-	string event;
-	int row;
+    static int nextId;
+    const int id;
+    string event;
+    int row;
+    float price;
+
+    static const int maxTickets = 5;
+    int ticketNumbers[maxTickets];
+    int numberTicket;
+
+    Date ticketDate;
 
 public:
-	Ticket();
-	Ticket(string event, int row);
+    Ticket();
+    Ticket(string event, int row);
 
-	int getId();
-	string getEvent();
-	int getRow();
+    void buyTicket(int ticketNumber);
 
-	void setEvent(string event);
-	void setRow(int row);
+    const int* getTicketNumber();
+    int getNumTickets();
+    int getId();
+    string getEvent();
+    int getRow();
+    Date getTicketDate();
 
-	void displayTicket();
+    void setTicketDate(Date date);
+    void setTicketNumber();
+    void setEvent(string event);
+    void setRow(int row);
 
-	friend std::ostream &operator<<(std::ostream &out, Ticket &ticket);
-	friend std::istream &operator>>(std::istream &in, Ticket &ticket);
+    void displayTicket();
+
+    friend std::ostream& operator<<(std::ostream& out, Ticket& ticket);
+    friend std::istream& operator>>(std::istream& in, Ticket& ticket);
 };
