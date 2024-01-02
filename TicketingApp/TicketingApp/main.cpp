8ee41c3584f08dccc5472b539750f9023f12e287
draghici05 <147331173+draghici05@users.noreply.github.com>
2024-01-02 19:25:04 +0200

    Update main.cpp

--- TicketingApp/TicketingApp/main.cpp
+++ TicketingApp/TicketingApp/main.cpp
@@ -3,8 +3,8 @@
 #include "Date.h"
 #include "Event.h"
 #include "Location.h"
-#include "Ticket.h"
 #include "Zone.h"
+#include "Ticket.h"
 
 using namespace std;
 
@@ -13,24 +13,65 @@ void Menu() {
 	cout << "-------------------------------\n";
 	cout << "-------- Ticketing App --------\n";
 	cout << "-------------------------------\n";
+	cout << "\n";
 
 	cout << " 1.Buy Ticket \n";
 	cout << " 2.Check Ticket \n";
 	cout << " 3.Modify Ticket \n";
 	cout << " 4.Exit the App \n";
+	cout << "\n";
+
+	cout << "-------------------------------\n";
+	cout << "Pick an option from the list: ";
 
 	do {
 		cin >> choice;
 		switch (choice) {
 		case 1:
-			cout << "Select the ticket: \n";
+			int ticketNumber;
+			cout << "Enter the ticket number: \n";
+			cin >> ticketNumber;
+			cout << "Ticket has been bought. \n";
 			break;
 		case 2:
 			cout << "The details of your ticket: \n";
+			ticket.displayTicket;
 			break;
 		case 3:
+			int modify;
 			cout << "Select the details that need to be modified: \n";
-			break;
+			cout << "1. Modify Event\n";
+			cout << "2. Modify Row\n";
+			cout << "3. Modify Date\n";
+			cout << "Choose an option: ";
+			cin >> modify;
+			
+			switch(modify) {
+			case 1:
+			{
+				string newEvent;
+				cout << "Enter the new event name: ";
+				cin.ignore();
+				getline(cin, newEvent);
+				ticket.setEvent(newEvent);
+				cout << "Event has been modified successfully";
+				break;
+			}
+			case 2:
+				int newRow;
+				cout << "Enter the new row: ";
+				cin >> newRow;
+				ticket.setRow(newRow);
+				cout << "Row has beed modified successfully";
+				break;
+			case 3:
+				Date newDate;
+				cout << "Enter the new date: ";
+				cin >> newDate;
+				ticket.setTicketDate(newDate);
+				cout << "Date has been modified successfully";
+				break;
+			}
 		case 4:
 			cout << "Closing the app... \n";
 			exit(0);
@@ -43,7 +84,8 @@ void Menu() {
 	while (choice != 4);
 }
 
-int main() {
-	
+int main(int argc, char** argv) {
+	Ticket ticket;
 	Menu();
+	return 0;
 }
