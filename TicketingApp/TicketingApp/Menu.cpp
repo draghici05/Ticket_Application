b4d28bb8b19af8e41d85a664cda27ac420d28b1d
draghici05 <147331173+draghici05@users.noreply.github.com>
2024-01-09 23:24:49 +0200

    Update Menu.cpp

--- TicketingApp/TicketingApp/Menu.cpp
+++ TicketingApp/TicketingApp/Menu.cpp
@@ -10,12 +10,15 @@
 #include "Zone.h"
 #include "Ticket.h"
 #include "ProcessFile.h"
-
+#include "Menu.h"
 
 using namespace std;
 
+//Could be worse, clearly not the best project but for a person who barely knows any coding it is something	
+
 Ticket ticket;
-Event event;
+//Event event;
+ProcessFile processFile;
 
 void saveToFile(vector<Ticket>& tickets, string& ticket_data) {
 	ofstream outFile(ticket_data, ios::binary);
@@ -29,8 +32,9 @@ void saveToFile(vector<Ticket>& tickets, string& ticket_data) {
 	outFile.close();
 }
 
-void Menu() {
-	int choice;
+void Menu::DisplayMenu() {
+	int choice;;
+
 	cout << "-------------------------------\n";
 	cout << "-------- Ticketing App --------\n";
 	cout << "-------------------------------\n";
@@ -47,20 +51,28 @@ void Menu() {
 
 	do {
 		cin >> choice;
+		cin.clear();
+		cin.ignore();
+
 		switch (choice) {
 		case 1:
 			event.chooseEvent();
-
+				
 			int ticketNumber;
 			cin >> ticketNumber;
-			cout << "Ticket has been bought. \n";
+			cout << "Ticket has been bought.";
+
 			if (!(cin >> ticketNumber)) {
 				cout << "Invalid input\n";
-				cin.clear();
-				cin.ignore();
 			}
+			else {
+				Ticket ticket;
+			}
+			ticket.setEvent(event.getName());  
+			ticket.setTicketDate(event.getDate());
+			ticket.setTicketNumber(ticketNumber);
+
 			ticket.buyTicket(event, ticketNumber);
-			cout << "The ticket has been bought.";
 			break;
 		case 2:
 			cout << "The details of your ticket: \n";
@@ -88,25 +100,25 @@ void Menu() {
 			}
 			case 2:
 				int newRow;
-				cout << "Enter the new row: ";
+				cout << "Enter the new row: \n";
 				cin >> newRow;
 				ticket.setRow(newRow);
-				cout << "Row has beed modified successfully";
+				cout << "Row has beed modified successfully \n";
 				break;
 			case 3:
 				Date newDate;
-				cout << "Enter the new date: ";
+				cout << "Enter the new date: \n";
 				cin >> newDate;
 				ticket.setTicketDate(newDate);
-				cout << "Date has been modified successfully";
+				cout << "Date has been modified successfully \n";
 				break;
 			}
 		case 4:
-			cout << "Closing the app... \n";
+			cout << "\n Closing the app... \n";
 			exit(0);
 			break;
 		default:
-			cout << "Pick an option from the list.";
+			cout << "\n Pick an option from the list.";
 		}
 	}
 
@@ -114,13 +126,18 @@ void Menu() {
 }
 
 int main(int argc, char** argv) {
+	
+	Menu menu;
+	ProcessFile processFile;
+	std::vector<Ticket> ticket;
 
-	vector<Ticket> tickets;
-	if (argc > 1) {
-		ProcessFile(argv[1]);  //Idea from https://stackoverflow.com/questions/3024197/what-does-int-argc-char-argv-mean
+	processFile.saveToFile(ticket, "output.bin");
+
+	if (argc > 1) {				//Idea from https://stackoverflow.com/questions/3024197/what-does-int-argc-char-argv-mean
+		processFile.fileProcess(argv[1]);  
 	}
 	else {
-		Menu();
+		menu.DisplayMenu();
 	}
 
 	return 0;
