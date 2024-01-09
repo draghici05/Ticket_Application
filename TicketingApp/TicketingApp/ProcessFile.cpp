970e18979f529a3a99a7ce5b60bcd2d46298fb0c
draghici05 <147331173+draghici05@users.noreply.github.com>
2024-01-09 23:25:03 +0200

    Update ProcessFile.cpp

--- TicketingApp/TicketingApp/ProcessFile.cpp
+++ TicketingApp/TicketingApp/ProcessFile.cpp
@@ -1,16 +1,33 @@
 #include "ProcessFile.h"
 #include <iostream>
 #include <fstream>
+#include <string>
 
-using namespace std;
-
-void ProcessFile(const char* ProiectOOP)
-{
-	ifstream inputFile(ProiectOOP);
-	
-	if (!inputFile.is_open()) {
-		cout << "Error for: " << ProiectOOP << endl;
+void ProcessFile::saveToFile(std::vector<Ticket>&ticket, const std::string& ticket_data) {
+	std::ofstream outFile(ticket_data, std::ios::binary);
+	if (!outFile.is_open()) {
+		std::cout << "Error" << std::endl;
 		return;
+	}
+	for (const Ticket& t: ticket) {
+		t.saveToFile(outFile);
+	}
+	outFile.close();
+
+}
+
+void ProcessFile::fileProcess(const std::string& data) {
+	
+	std::ifstream inputFile(data);
+
+	if (!inputFile.is_open()) {
+		std::cout << "Error when opening file" << data << std::endl;
+		return;
+	}
+
+	std::string line;
+	while (std::getline(inputFile, line)){
+		std::cout << "Processing line: " << line << std::endl;
 	}
 
 	inputFile.close();
